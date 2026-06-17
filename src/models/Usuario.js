c5e1b939749fbc385733391a26ea93330a462cf5
Fernando Pires <fernandopires182@hotmail.com>
2026-06-16 21:41:58 -0300

    Update Usuario.js

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,1 +1,22 @@
+class Usuario {
+    constructor({ id, nome, email, data_nascimento, senha_hash, saldo }) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = data_nascimento;
+        this.senhaHash = senha_hash;
+        this.saldo = parseFloat(saldo) || 0;
+    }
 
+    toJSON() {
+        return {
+            id: this.id,
+            nome: this.nome,
+            email: this.email,
+            dataNascimento: this.dataNascimento,
+            saldo: this.saldo
+        };
+    }
+}
+
+module.exports = Usuario;
