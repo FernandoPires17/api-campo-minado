2afa5d9eb11937d4134f99fe0e55353793e5505b
Fernando Pires <fernandopires182@hotmail.com>
2026-06-16 21:42:16 -0300

    Update Jogo.js

--- src/models/Jogo.js
+++ src/models/Jogo.js
@@ -1,1 +1,24 @@
+class Jogo {
+    constructor({ id, usuario_id, valor_aposta, premio_atual, diamantes_encontrados, status, tabuleiro }) {
+        this.id = id;
+        this.usuarioId = usuario_id;
+        this.valorAposta = parseFloat(valor_aposta);
+        this.premioAtual = parseFloat(premio_atual) || 0;
+        this.diamantesEncontrados = diamantes_encontrados || 0;
+        this.status = status || 'EM_ANDAMENTO';
+        this.tabuleiro = tabuleiro;
+    }
 
+    toJSON() {
+        return {
+            id: this.id,
+            valorAposta: this.valorAposta,
+            premioAtual: this.premioAtual,
+            diamantesEncontrados: this.diamantesEncontrados,
+            status: this.status,
+            tabuleiro: this.tabuleiro
+        };
+    }
+}
+
+module.exports = Jogo;
